f16a706d1ef0309236ed78251a962ff03afcdd95
mvanderuitenbeek <mvanderuitenbeek@letsconstruct.nl>
2018-06-21 23:26:16 +0200

    Added some rough code here

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import turtle
+
 y = 6
+print(8 / 3)
+y = int(input("wat is de temp? "))
+# print(p + 1)
+
+if (y < 10):
+    print("yes")
+else:
+    print("oh dear")
+
+while (y < 10):
+    print("cooling down...")
+    y = y + 1
+
+print("final temp:")
+print(y)
